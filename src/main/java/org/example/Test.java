022c1bdd71fc1c6ea18b1af6d5b9d2c394e4e483
georgesco11 <georgestudent1998@gmail.com>
2026-04-26 23:25:36 +0400

    create two text file

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Test {
+    public static void main(String[] args) {
+        int studentAge1 = 128;
+        float studentAge2 = 34.4f;
+        char certificateGrade = 'A';
+        boolean c = 10==11;
+        System.out.println(c);
+        System.out.println(studentAge1);
+        System.out.println(certificateGrade);
+        System.out.println(studentAge2);
+    }
+}
